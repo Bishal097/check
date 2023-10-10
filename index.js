8dce12b9e64d2acdebd1e548a40349f07a05feb1
Bishal Bhattacharjee <103323091+Bishal097@users.noreply.github.com>
2023-10-10 05:26:40 +0000

    CONVERSION OF DATA TYPE

---
+++ index.js
@@ -0,0 +1,44 @@
+/*console.log("Bishal");
+const account_id=102255;
+let account_email="bishalbhattacharjee55@gmail.com";
+var acount_password="12345";
+account_city="jaipur"
+
+//account_id=2
+account_email="bishal@gmail.com";
+acount_password="123";
+account_city="Asansol";
+console.table([account_email,acount_password,account_city]);
+
+"use strict";
+//null=stand alone value and data type matlab khali hai
+//unndefined matlab abhi value assign nhi hui hai
+//symbol used in react for unique representation
+//object
+let name="null";
+console.log(typeof null)
+console.log(typeof char);
+console.log(typeof name);
+
+
+
+let score="34";
+let s="33sbc";
+let j=undefined;
+let b="Bishal";
+let y=null;
+let k=Number(score);
+let p=Number(s);
+let i=Number(j);
+let h=Number(b);
+let t= Number(y);
+
+console.table([typeof score,typeof s,typeof j,typeof b, typeof y])
+console.table([typeof k,typeof p,typeof i,typeof h, typeof t])
+console.table([k,p,i,h,t]);
+//
+FOR BOOLEAN 
+1->TRUE ,0->FALSE
+" "->FALSE
+"BISHAL"->TRUE
+*/
